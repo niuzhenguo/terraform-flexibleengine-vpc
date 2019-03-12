808392d9a54f9bfd77312fc29b1a8f419a2f525d
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-03-12 10:37:24 +0800

    update variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 #define VPC variable
-variable "vpc_name" {}
+variable "vpc_name" {
+  description = "The name used to create a new VPC."
+  default     = "TF-VPC"
+}
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "The cidr block used to create a new VPC."
+  default     = "172.16.0.0/12"
+}
